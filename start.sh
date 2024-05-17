c5a529969cf7d3a7b35f24f018255c102a9a5d56
samuel <rebuuula@gmail.com>
2024-05-17 00:01:28 -0300

    start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+# Using yarn
+# Create a new React app with TypeScript template
+yarn create react-app espguitars --template typescript
+
+# Change directory to the newly created app
+cd espguitars
+
+# Add sass
+yarn add sass
+
+# Start the development server
+yarn start
